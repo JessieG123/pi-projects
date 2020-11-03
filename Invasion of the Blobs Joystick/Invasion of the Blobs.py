c0d00d4214f46f500f614776ad21670adf6a3845
JessieG123 <jessieguo863@gmail.com>
2020-11-03 10:47:42 -0800

    test

--- Invasion of the Blobs Joystick/Invasion of the Blobs.py
+++ Invasion of the Blobs Joystick/Invasion of the Blobs.py
@@ -489,7 +489,7 @@ class Game:
                         return
                     if e.key == K_p:
                         self.paused ^= 1
-            #TODO: change to joystick
+            #TODO: change to joystickk
             if down():
                 option = 2
             if up():
