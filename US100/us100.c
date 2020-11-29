fe86aa24eb05c340e48a07c43cb741838f9cfa76
JessieG123 <jessieguo863@gmail.com>
2020-11-29 12:56:01 -0700

    setup us100 files

---
+++ US100/us100.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stddef.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <sys/mman.h>
+#include <unistd.h>
+#include <bcm_host.h>
+#include <time.h>
+
+
+#define BLOCK_SIZE 4096
